6a4938ba5e5c07bcf14a206bfb2a038aa3819d0f
Adriana Disano <adrianadisano@gmail.com>
2018-08-16 16:19:47 -0500

    Add project files.

---
+++ HoroscopeHomework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HoroscopeHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
